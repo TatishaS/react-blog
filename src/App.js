b6729747b8e4fc8f5aa9104edef5881c04877656
Tati <snow_white_girl@bk.ru>
2022-03-14 15:36:25 +0300

    Initial commit: basic project structure is ready

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img className="logo" alt="logo" />
+      </header>
+      <main className="page page--main"></main>
+    </div>
+  );
+}
+
+export default App;
