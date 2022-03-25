f6f57fe007242f8147e49b9771d6f3f439cbdd8c
Tati <snow_white_girl@bk.ru>
2022-03-25 13:49:18 +0300

    =fixes

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 app.use(
   fileUpload({
     createParentPath: true,
