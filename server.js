d0002282d78abb29d340c123f484e2d55eef5168
TatishaS <snow_white_girl@bk.ru>
2022-05-25 11:47:19 +0300

    chanded path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
-const { initRoutes } = require('./blog-api/routes');
+const { initRoutes } = require('./server-api/routes');
 const morgan = require('morgan');
 
 require('dotenv').config();
