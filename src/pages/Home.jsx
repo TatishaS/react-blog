38899048c2c6eadfc3525f185d6c7c0e43a5b4e8
TatishaS <snow_white_girl@bk.ru>
2023-11-02 08:39:12 +0300

    Layout changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import authorImg from '../assets/img/author.jpg';
+import authorImg from '../assets/img/author1.jpg';
 
 export const Home = () => {
   return (
     <>
       <section className="section about">
         <h1 className="about__title">Tati Sidorova</h1>
-        <h2 className="about__subtitle">Блог юного фронтенд-разработчика</h2>
+        <h2 className="about__subtitle">My lovely development blog</h2>
         <img
           className="about__img"
           src={authorImg}
@@ -15,13 +15,14 @@ export const Home = () => {
           width="584"
           height="422"
         />
-        <h3 className="about__content-title">Обо мне</h3>
+        <h3 className="about__content-title">About</h3>
         <p className="about__text">
-          In this blog I plan to post my experince in testing different
-          frontend, and - who knows? - maybe backend instruments. Also my
-          thoughts, moments and impressions during my developer journey. You are
-          also welcome to share your opinion: you can make a post after
-          authorization or leave your comment under my post.
+          I'm a dedicated and inspired frontend dev. In this blog I plan to post
+          my experince in testing different frontend, and - who knows? - maybe
+          backend instruments. Also my thoughts, moments and impressions during
+          my developer journey. You are also welcome to share your opinion: you
+          can make a post after authorization or leave your comment under my
+          post.
         </p>
       </section>
     </>
